128f2593c213fff35d01c63fa43a650b73cde179
liangtianpei <liangtianpei@jd.com>
2018-05-21 16:40:15 +0800

    feat: travis ci配置

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ if [ "$1" = "false" ]
 # not a pull request, deploy to github pages
 then ( cd dist
   git init
-  git config user.name "admin"
-  git config user.email "admin@nervjs.com"
+  git config user.name "ltp11"
+  git config user.email "823285635@qq.com"
   git add .
   git commit -m "Deployed to Github Pages [skip ci]"
   git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
