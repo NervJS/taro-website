ff92a5d51a2c4dcc0f04028c6d16e1537ae92631
luckyadam <weitaozsh@gmail.com>
2018-05-25 17:25:01 +0800

    feat: 文本优化

--- src/locales/zh-cn.js
+++ src/locales/zh-cn.js
@@ -1,7 +1,7 @@
 export default {
   home: '首页',
   docs: '文档',
-  banner_desc: '一套遵循React语法规范的多端开发解决方案',
+  banner_desc: '一套 React 语法规范的多端开发解决方案',
   getting_started: '起步',
   features: '特性 Features',
   support: '支持 Support',
@@ -10,17 +10,17 @@ export default {
   examples: '代码示例 Examples',
   
   size: '多端运行',
-  size_desc: '一键生成可以在微信小程序/H5/ReactNative等端运行的代码',
+  size_desc: '一键生成可以在微信小程序/H5/ReactNative 等端运行的代码',
   perf: '语法风格',
-  perf_desc: '采用React语法标准，支持JSX书写，让代码更具表现性',
+  perf_desc: '采用React语法标准，支持 JSX 书写，让代码更具表现性',
   ecos: '组件化',
   ecos_desc: '支持组件化开发，让代码可以复用，让功能开发更加清晰',
   isom: 'NPM包管理',
   isom_desc: '支持使用NPM进行管理第三方依赖，自动分析包之间依赖关系，自动提取依赖文件',
   compat: 'ES Next',
-  compat_desc: '基于babel编译，用户可以自行选择使用的ES版本，同时引入 @tarojs/async-await 包即可使用 async function 语法',
+  compat_desc: '基于 babel 编译，用户可以自行选择使用的ES版本，同时引入 @tarojs/async-await 包即可使用 async function 语法',
   battle: '资源引用',
-  battle_desc: '可以很方便地引用本地图片、字体、JSON文件等静态资源，taro会自动分析文件依赖关系，并替换相应路径',
+  battle_desc: '可以很方便地引用本地图片、字体、JSON 文件等静态资源，taro 会自动分析文件依赖关系，并替换相应路径',
   
   support1: '微信小程序',
   support2: 'H5',
@@ -38,7 +38,7 @@ export default {
   js_framework:
     '这是个简单的测试框架性能的例子。我们选取了一些市面上常用的JS框架，通过随机创建一个大型的表格，对比他们的运行速率，可以看到Nerv执行完所有用例的时间远低于其他框架。',
   memory:
-    '与测试框架性能的测试用例相似，这次的对比维度是内存占用率，可发现，nerv在此项中的表现也相当的优秀。',
+    '与测试框架性能的测试用例相似，这次的对比维度是内存占用率，可发现，nerv 在此项中的表现也相当的优秀。',
   ui_bench:
     '本例子使用UI Benchmark对比若干JS框架的渲染速度，相同测试用例下，nerv的渲染耗时最短。',
   db_monster:
